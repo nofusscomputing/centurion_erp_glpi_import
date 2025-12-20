a18dd6608a0cff4f79d7e84cfed6b802c62d3d22
Jon <jonathon.lockwood@networkedweb.com>
2025-12-20 14:02:08 +0930

    feat(glpi): Add DB router to point to legacy tables
    ref: #5 #4

---
+++ src/db_router.py
@@ -0,0 +1,27 @@
+class GlpiImportRouter:
+
+    db_alias = "glpi_import"
+
+
+    def db_for_read(self, model, **hints):
+
+        if model._meta.app_label == "glpi_import":
+            return self.db_alias
+
+        return None
+
+
+    def db_for_write(self, model, **hints):
+
+        if model._meta.app_label == "glpi_import":
+            return None
+
+        return None
+
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+
+        if db == self.db_alias:
+            return False
+
+        return None
